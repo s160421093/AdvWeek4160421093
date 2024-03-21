b2bc287ffb56caf3e5ca0fb310143eeef074c7d8
s160421093 <s160421093@student.ubaya.ac.id>
2024-03-21 14:23:44 +0700

    init week5
    adas error

--- app/src/main/java/com/ubaya/advweek4160421093/model/Model.kt
+++ app/src/main/java/com/ubaya/advweek4160421093/model/Model.kt
@@ -1,8 +1,14 @@
 package com.ubaya.advweek4160421093.model
+
+import com.google.gson.annotations.SerializedName
+
 data class Student(
-    var id:String?,
-    var name:String?,
-    var bod:String?,
-    var phone:String?,
-    var photoUrl:String?
+    val id:String?,
+    @SerializedName("student_name")
+    val name:String?,
+    @SerializedName("birth_of_date")
+    val bod:String?,
+    val phone:String?,
+    @SerializedName("photo_url")
+    val photoUrl:String
 )
